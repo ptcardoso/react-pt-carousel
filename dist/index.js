d9da41c5d5747c2db1f5beb2b3f08c8555c11ff8
Pedro Cardoso <pmtcardoso@gmail.com>
2018-11-28 21:01:14 +0000

    Build

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+import { Carousel } from './Carousel.tsx';
+import { PreviousButton, NextButton } from './Controllers.tsx';
+import { Indicators } from './Indicators.tsx';
+
+export { Carousel, PreviousButton, NextButton, Indicators };
